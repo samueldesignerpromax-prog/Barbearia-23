f7a88e7603cf5b36fbca069455e01f0fd4d079a8
Samuel Tech IA <samueldesignerpromax@gmail.com>
2026-08-03 20:56:58 -0300

    Initialize API module with axios configuration

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_URL = 'https://barbearia-backend-7smg.onrender.com';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
+export { API_URL };
